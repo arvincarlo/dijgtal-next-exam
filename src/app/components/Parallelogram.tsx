a0579fc60c2162b9be4fff0f809dfaf3421ea8bb
Arvin Cris <arvincris97@gmail.com>
2025-01-24 10:00:07 +0800

    added background and parallelogram shape

---
+++ src/app/components/Parallelogram.tsx
@@ -0,0 +1,5 @@
+export default function Parallelogram() {
+    return (
+        <div className="parallelogram fixed inset-0 z-0"></div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Parallelogram from "./components/Parallelogram"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-300">
-      Hello world!
-    </h1>
+    <div className="relative z-10 text-white text-center min-h-screen flex items-center justify-center bg-stone-800">
+      <h1 className="text-3xl font-bold underline text-blue-300">
+        Hello world!
+      </h1>
+      <Parallelogram></Parallelogram>
+    </div>
   )
 }
